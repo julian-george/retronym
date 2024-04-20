0f8f73df98d518d57536403b0928a3980228b21f
Julian George <juliancgeorge@gmail.com>
2024-04-20 18:13:39 -0400

    add logout

--- src/pages/FeedPage.tsx
+++ src/pages/FeedPage.tsx
@@ -1,43 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, Dispatch, SetStateAction, useCallback } from "react";
+import { useUser } from "../context/userContext";
 
-const Sidebar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
+const Sidebar: React.FC<{
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}> = ({ isOpen, setIsOpen }) => {
+  const { logout } = useUser();
   return (
-    <div className="flex h-full">
-      {/* Sidebar */}
+    <>
       <div
-        className={`bg-gray-800 text-white w-64 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform ${
+        className={`bg-gray-200 w-64 absolute inset-y-0 left-0 flex flex-col transform ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         } transition duration-200 ease-in-out`}
       >
-        {/* Sidebar content */}
-        <h1 className="text-white text-3xl font-semibold">Brand</h1>
-        <ul>
-          <li className="padding-4 hover:bg-gray-700">Home</li>
-          <li className="padding-4 hover:bg-gray-700">About</li>
-          <li className="padding-4 hover:bg-gray-700">Services</li>
-          <li className="padding-4 hover:bg-gray-700">Contact</li>
-        </ul>
+        <div className="flex-1"></div>
+        <button onClick={() => logout()}>Log Out</button>
       </div>
 
       {/* Overlay to close sidebar on mobile */}
       <div
-        className={`fixed inset-0 bg-black opacity-50 z-20 transition-opacity ${
+        className={`fixed inset-0 bg-black opacity-0 z-20 left-64 transition-opacity ${
           isOpen ? "opacity-50 visible" : "opacity-0 invisible"
         }`}
         onClick={() => setIsOpen(false)}
       ></div>
-    </div>
+    </>
   );
 };
 
 function FeedPage() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const toggleSidebar = useCallback(() => {
+    setSidebarOpen((prev) => !prev);
+  }, [setSidebarOpen]);
   return (
     <>
-      <Sidebar />
-      <div className="flex justify-center items-center">
-        <h1>Retronym</h1>
+      <Sidebar isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />
+      <div className="flex items-center w-full">
+        <div className="flex-1">
+          <div className="text-left" onClick={toggleSidebar}>
+            Sidebar
+          </div>
+        </div>
+        <div className="flex-1">
+          <h1 className="text-center">Retronym</h1>
+        </div>
+        <div className="flex-1"></div>
       </div>
     </>
   );
